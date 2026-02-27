ec54b83443446cd46edf2e80c5e0008ed661f269
ayshasurayya12 <ishaaaaashh@gmail.com>
2026-02-27 10:26:08 +0530

    difference of two array

---
+++ difference_twoArrays.py
@@ -0,0 +1,23 @@
+# Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+# answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+# answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+# Note that the integers in the lists may be returned in any order.
+
+
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        diff1 = list(set1-set2)
+        diff2 = list(set2-set1)
+        return [diff1,diff2]
+
+s = Solution()
+print(s.findDifference([1,2,3],[2,4,6]))
